221d55b45fd9246054bc7a046803626c0501ead6
Tsuneki Umeda <umedat@amazon.co.jp>
2020-12-30 20:03:57 +0900

    handlebars

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 const path = require('path')
 const express = require('express')
 
-console.log(__dirname)
-
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Tsuneki',
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Tsuneki',
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'About',
+    name: 'Tsuneki',
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send([

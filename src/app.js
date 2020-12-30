47455c01f6775e5cec5bfbe6f50ff9792514fa8a
Tsuneki Umeda <umedat@amazon.co.jp>
2020-12-30 13:43:53 +0900

    using static files

--- src/app.js
+++ src/app.js
@@ -7,26 +7,6 @@ const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
-app.get('', (req, res) => {
-  res.send('<h1>Weather</h1>')
-})
-
-// app.get('/help', (req, res) => {
-//   res.send([
-//     {
-//       name: 'andrew',
-//       age: 27,
-//     },
-//     {
-//       name: 'sarah',
-//     },
-//   ])
-// })
-
-// app.get('/about', (req, res) => {
-//   res.send('<h1>About page</h1>')
-// })
-
 app.get('/weather', (req, res) => {
   res.send([
     {
